79af325cccc884886dd8079b8c4149fa49f1ce73
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-08-14 22:47:25 +0700

    feat: setup template enginge, tailwindcss, and tailwind prettier.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { AppService } from './app.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return { message: this.appService.getHello() };
   }
 }
